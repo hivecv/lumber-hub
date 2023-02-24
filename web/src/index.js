a1ea6a83453cd2249804225661eda92fd7af1720
Łukasz Piłatowski <lukpila29@gmail.com>
2023-02-25 00:16:24 +0100

    prepare a landing page

--- web/src/index.js
+++ web/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import store from './redux';
-import 'antd/dist/antd.min.css';
+import 'antd/dist/reset.css';
+import './index.css';
+import {ConfigProvider, theme} from "antd";
 
+const customTheme = {
+  token: {
+    colorPrimary: '#86695d',
+    algorithm: theme.darkAlgorithm,
+  },
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ConfigProvider theme={customTheme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ConfigProvider>
 );
